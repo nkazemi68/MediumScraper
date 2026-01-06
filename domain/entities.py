493f9bf0c4f94e33d43bf2130dc0dcefa2c5f41e
nkazemi68 <it.nkazemi@gmail.com>
2026-01-06 14:17:49 +0330

    feat(domain): define core domain entities

---
+++ domain/entities.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from typing import Optional, Dict, List
+
+
+@dataclass
+class Topic:
+    topic_id: str
+    title: str
+
+
+@dataclass
+class User:
+    user_id: str
+    username: str
+    name: Optional[str] = None
+    bio: Optional[str] = None
+
+    image_id: Optional[str] = None
+    subdomain: Optional[str] = None
+    is_book_author: bool = False
+    membership_data: Optional[Dict[str, str]] = None
+
+    follower_count: int = 0
+    following_count: int = 0
+
+    user_meta: Optional[Dict[str, str]] = None
+
+    about: Optional[str] = None
+
+
+@dataclass
+class Post:
+    post_id: str
+    author_id: str
+    title: str
+    published_at: str
+    subtitle: Optional[str] = None
+    clap_count: Optional[int] = None
+    responses_count: Optional[int] = None
+    reading_time: Optional[float] = None
+    collection_id: Optional[int] = None
+    content: Optional[Dict] = None
+
+
+@dataclass
+class Follower:
+    user_id: str
+    followed_user_id: str
